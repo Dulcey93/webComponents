2fdc472e95f2cb2e0feb89bb8297c13ff695ac80
LuisSantiagoDulceyRamirezCampus <ldulcey93@unab.edu.co>
2023-05-04 21:47:28 -0500

    feat(After Classroom): :sparkles: New Code
    New code added after the upload in Classroom

--- components/myForm/my-form.js
+++ components/myForm/my-form.js
@@ -2,12 +2,12 @@ import config from "../../config.js";
 
 export default class MySelect extends HTMLElement {
     static url = import.meta.url;
-    static async components(){
+    static async components() {
         return await (await fetch(config.endPoint(MySelect.url))).text();
     }
     constructor() {
         super();
-        this.attachShadow({mode: "open"});
+        this.attachShadow({ mode: "open" });
     }
     handleEvent(e) {
         (e.type === "submit") ? this.sendMessage(e) : undefined;
@@ -15,13 +15,21 @@ export default class MySelect extends HTMLElement {
     }
 
     sendMessage(e) {
-        //El e.target apunta al formulario
-        console.log(e.target);
-        //El FormData es un objeto que contiene todos los datos del formulario en forma de clave valor
-        const data = Object.fromEntries(new FormData(e.target));
-        console.log(data);
-        /* console.log(e); */
         e.preventDefault();
+        const data = Object.fromEntries(new FormData(e.target).entries());
+  
+        // Obtener el valor del input de tipo "text"
+        this.inputValue = data.input1;
+        
+        // Obtener el valor del input de tipo "radio"
+        const radioInput = e.target.querySelector('input[name="radio"]:checked');
+        this.radioValue = radioInput.value;
+        
+        // Cambiar el textContent del label con el valor del input de tipo "text"
+        this.label = this.shadowRoot.querySelector('label[name="inputLabel"]');
+        this.label.textContent = this.inputValue;
+        console.log(data);
+        e.target.reset();
     }
 
     connectedCallback() {
